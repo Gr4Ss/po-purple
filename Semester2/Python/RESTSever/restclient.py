ab42c894533b70d0994b813248dd7b97f7209ed8
wout276 <wout276@live.be>
2016-02-25 16:40:40 +0100

    basis spookserver, self-calling fixes RESTserver

--- Semester2/Python/RESTSever/restclient.py
+++ Semester2/Python/RESTSever/restclient.py
@@ -9,7 +9,7 @@ def generate_secret_key(length):
         result.append(charac)
     return result
 
-class RestClient:
+class RestClient(object):
     def __init__(self,root,teamname):
         self.__root = root
         self.__secret_key = generate_secret_key(16)
@@ -23,27 +23,31 @@ class RestClient:
             return True
         else:
             return False
-    def get_map():
+    def get_map(self):
         resp = requests.get(self.__root + "/map")
         return json.loads(resp)
-    def get_parcels():
+    def get_parcels(self):
         resp = requests.get(self.__root + "/parcels")
         return json.loads(resp)
-    def claim_parcel(parcel_nb):
+    def claim_parcel(self, parcel_nb):
         if self.__registed:
             resp = requests.put(self.__root + "/robots/"+self.__name+"/claim/" + str(parcel_nb),data={"key":self.__key})
             return resp.text == 'OK'
         else:
             raise Error('Please add your team first.')
-    def deliver_parcel(parcel_nb):
+    def deliver_parcel(self, parcel_nb):
         if self.__registed:
             resp = requests.put(self.__root + "/robots/"+self.__name+"/delivered/" + str(parcel_nb),data={"key":self.__key})
             return resp.text == 'OK'
         else:
             raise Error('Please add your team first.')
-    def update_position(from_node,to_node):
+    def update_position(self, from_node,to_node):
         if self.__registed:
             resp = requests.put(self.__root + "/positions/"+self.__name+"/"+ str(from_node)"/"+str(to_node),data={"key":self.__key})
             return resp.text == 'OK'
         else:
             raise Error('Please add your team first.')
+    def get_postions(self):
+	pass
+    def get_teamname(self):
+	return self.__name
