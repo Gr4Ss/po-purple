4d8e7aa77850121fabb9d637defc101fedaeeae1
Agten Agten <arne_agten_36@hotmail.com>
2016-04-26 18:56:48 +0200

    removed epic bug

--- Code in progress/Website/Static/network.js
+++ Code in progress/Website/Static/network.js
@@ -1,161 +1,20 @@
-/*var Dataset = {
-	"vertices": [
-		[1, {"origin": 3, "straight": 2, "left": 4}],
-		[2, {"origin": 1, "straight": 3, "left": 4}],
-		[3, {"origin": 2, "straight": 1, "left": 4}],
-		[4, {"origin": 3, "straight": 1, "left": 2}]
-	],
-	"edges": [
-		[1, 2, 0.3],
-		[1, 3, 0.5],
-		[3, 1, 0.5],
-		[2, 3, 0.1],
-		[3, 2, 0.1],
-		[3, 4, 0.7],
-		[4, 2, 0.3],
-		[4, 1, 0.8]
-	]
-};*/
-
-/**var Dataset = {
-	"vertices": [
-		[1, {"origin": 6, "straight": 2}],
-		[2, {"origin": 1, "straight": 3, "left": 4}],
-		[3, {"origin": 2, "straight": 7, "left": 9}],
-		[4, {"origin": 2, "straight": 5}],
-		[5, {"origin": 4, "left": 6, "right": 10}],
-		[6, {"origin": 5, "straight": 1}],
-		[7, {"origin": 3, "straight": 8}],
-		[8, {"origin": 7, "straight": 9}],
-		[9, {"origin": 8, "left": 3, "right": 10}],
-		[10, {"origin": 9, "straight": 5}],
-	],
-	"edges": [
-		[1, 2, 0.3],
-		[2, 1, 0.3],
-		[2, 3, 0.3],
-		[3, 2, 0.3],
-		[6, 1, 0.6],
-		[2, 4, 0.3],
-		[4, 5, 0.3],
-		[3, 9, 0.3],
-		[9, 3, 0.3],
-		[3, 7, 0.3],
-		[7, 3, 0.3],
-		[7, 8, 0.3],
-		[8, 9, 0.3],
-		[9, 8, 0.3],
-		[9, 10, 0.3],
-		[10, 9, 0.3],
-		[5, 10, 0.3],
-		[10, 5, 0.3],
-		[5, 6, 0.3],
-		[6, 5, 0.3],
-	]
-}**/
-
-/*Dataset for the demo*/
-/*var Dataset = {
-	"vertices": [
-		[1, {"origin": 1, "straight": 2}],
-		[2, {"origin": 1, "straight": 3}],
-		[3, {"origin": 2, "straight": 11, "right": 4}],
-		[4, {"origin": 3, "straight": 5, "left": 10}],
-		[5, {"origin": 4, "straight": 15, "right": 6}],
-		[6, {"origin": 5, "straight": 7, "left": 15}],
-		[7, {"origin": 6, "straight": 8, "left": 15}],
-		[8, {"origin": 7, "straight": 9, "left": 15, "right": 12}],
-		[9, {"origin": 8, "straight": 10, "left": 5, "right": 14}],
-		[10, {"origin": 9, "straight": 11, "left": 4, "right": 14}],
-		[11, {"origin": 10, "straight": 13, "left": 3, "right": 12}],
-		[12, {"origin": 11, "left": 13, "right": 8}],
-		[13, {"origin": 12, "straight": 11}],
-		[14, {"origin": 9, "straight": 10}],
-		[15, {"origin": 5, "straight": 7, "left": 8, "right": 6}],
-	],
-	"edges": [
-		[1, 2, 0.3],
-		[2, 3, 0.3],
-		[3, 4, 0.3],
-		[4, 5, 0.3],
-		[5, 6, 0.4],
-		[6, 7, 0.4],
-		[7, 8, 0.4],
-		[8, 9, 0.3],
-		[9, 10, 0.1],
-		[10, 11, 0.4],
-		[11, 12, 0.3],
-		[12, 13, 0.3],
-		[13, 11, 0.4],
-		[4, 10, 0.4],
-		[5, 9, 0.4],
-		[5, 10, 0.3],
-		[5, 15, 0.3],
-		[6, 15, 0.3],
-		[7, 15, 0.3],
-		[8, 15, 0.3],
-		[8, 12, 1],
-		[10, 14, 0.2],
-		[9, 14, 0.2],
-		[11, 3, 0.3],
-	]
-}*/
-
-/*var Dataset = {
-	"vertices": [
-		[0, {"origin": 1, "left": 2, "right": 5}],
-		[1, {"origin": 0, "straight": 2, "left": 4}],
-		[2, {"origin": 0, "left": 1, "right": 3}],
-		[3, {"origin": 2, "left": 4, "right": 7, "straight": 8}],
-		[4, {"origin": 1, "right": 3, "left": 5, "straight": 8}],
-		[5, {"origin": 0, "right": 4, "left": 6}],
-		[6, {"origin": 5, "straight": 7, "right": 8}],
-		[7, {"origin": 3, "right": 6, "straight": 8}],
-		[8, {"origin": 3, "left": 4, "straight": 6, "right": 7}]
-	],
-	"edges": [
-		[0, 1, 1.0],
-		[0, 2, 4.0],
-		[1, 0, 1.0],
-		[1, 2, 2.0],
-		[1, 4, 4.0],
-		[2, 0, 4.0],
-		[2, 1, 2.0],
-		[2, 3, 5.0],
-		[3, 2, 5.0],
-		[3, 4, 2.0],
-		[3, 7, 4.0],
-		[3, 8, 1.5],
-		[4, 1, 4.0],
-		[4, 5, 2.0],
-		[4, 8, 1.5],
-		[5, 0, 5.0],
-		[5, 4, 2.0],
-		[5, 6, 4.0],
-		[6, 5, 4.0],
-		[6, 7, 4.0],
-		[6, 8, 1.5],
-		[7, 3, 4.0],
-		[7, 6, 4.0],
-		[7, 8, 1.5],
-		[8, 3, 1.5],
-		[8, 4, 1.5],
-		[8, 6, 1.5],
-		[8, 7, 1.5]
-	]
-}
-*/
-
 var Dataset = [];
+var first = true;
 
+/* Gets the dataset from the server and converts it to a usable one for vis.js*/
 $.getJSON("http://localhost:9000/map", function(data) {
 	Dataset = data;
 	console.log(data);
 	convertDataSet();
 	network.on("stabilized", function(params) {
+		
 		network.fit();
-		colorAllPositions;
-		makeExternalLegend;
+		if (first) {
+			console.log("stabilized");
+			colorAllPositions();
+			first = false;
+		}
+		makeExternalLegend();
 	});
 })
 
@@ -278,24 +137,13 @@ function changeTeamPosition(carTeam) {
 	}
 }
 
-/* Changes the position of a team */
-function changePositionTeam(prevPosition, newPosition, teamName) {
-	pass;
-};
-
 
 //////////// SETTING UP NETWORK ////////////
-
-/* Converts the dataset to a new one usable in vis */
-//convertDataSet();
 
 /* Setup a new network */
 var network = new vis.Network(container, data, options);
 
-//changeNodeColor(1, '#ababab');
-//changeEdgeColor(3, 4, '#ab78ab');
-
-myPositions = "haha";
+var myPositions = "haha";
 
 carPrevPosMap = [];
 carPosMap = [];
@@ -303,12 +151,10 @@ carHashMap = {};
 
 function colorAllPositions() {
 	$.getJSON("http://localhost:9000/positions", function(data) {
-		console.log(data);
 		myPositions = data;
 		for (i in myPositions["positions"]){
 			carPosMap[i] = [myPositions["positions"][i][0], myPositions["positions"][i][1], myPositions["positions"][i][2]];
 			carHashMap[hashCode(myPositions["positions"][i][0])] = myPositions["positions"][i][0];
-			console.log(carPosMap[i]);
 		}
 		if (carPrevPosMap.length == 0) {
 			for (i in myPositions["positions"]) {
@@ -327,7 +173,6 @@ function colorAllPositions() {
 					changeEdgeColor(carPosMap[car][2], carPosMap[car][1], hexToRgbA(intToRGB(hashCode(carPosMap[car][0]))));
 				}
 			} else {
-				console.log("elsing2");
 				if (carPosMap[car][1] == carPosMap[car][2]) {
 					changeNodeColor(carPrevPosMap[car][1], hexToRgbA(intToRGB(hashCode(carPosMap[car][0]))));
 					changeEdgeColor(carPrevPosMap[car][1], carPrevPosMap[car][2], "#3399ff");
@@ -339,10 +184,6 @@ function colorAllPositions() {
 			}
 		}
 		carPrevPosMap = carPosMap;
-		
-		console.log(carHashMap);
-		
-		
 	});
 };
 
@@ -379,6 +220,10 @@ function hexToRgbA(hex){
 
 function makeExternalLegend() {
 	var legendDiv = document.getElementById("mylegend");
+	var resetNetworkButton = document.getElementById("resetnetwork");
+	var resetdesc = document.getElementById("resetdesc");
+	resetnetwork.style.width = 100/(carPosMap.length+1) + '%';
+	resetdesc.style.float = "left";
 	for (car in carPosMap) {
 		var containerDiv = document.createElement("div");
 		var descriptionDiv = document.createElement("button");
@@ -393,7 +238,7 @@ function makeExternalLegend() {
 		containerDiv.appendChild(descriptionDiv);
 		
 		containerDiv.style.width = "100%";
-		descriptionDiv.style.width = 100/carPosMap.length + '%';
+		descriptionDiv.style.width = 100/(carPosMap.length + 1) + '%';
 		descriptionDiv.style.float = 'left';
 		descriptionDiv.style.backgroundColor = hexToRgbA(intToRGB(hashCode(carPosMap[car][0])));
 		
@@ -441,6 +286,15 @@ function zoomToID(teamName) {
 	}
 } 
 
+var stabilized = false;
+/*while (!stabilized) {
+	network.on("stabilize", function() {
+		colorAllPositions();
+		makeExternalLegend();
+		stabilized = true;
+	});
+}*/
+
 
 //////////// MAINTAINING THE NETWORK ///////
 
@@ -459,6 +313,10 @@ function fitToContainer(canvas){
   canvas.height = canvas.offsetHeight;
 }
 
+$("#resetdesc").click(function(params) {
+	network.fit();
+})
+
 String.prototype.hashCode = function(){
 	var hash = 0;
 	if (this.length == 0) return hash;
@@ -469,37 +327,6 @@ String.prototype.hashCode = function(){
 	}
 	return hash;
 }
-
-
-/**
-setInterval(function(){ 
-	prevPos = carPos;
-	randVar = Math.random();
-	if (carPos == 1) {
-		carPos = 2;
-	} else if (carPos == 15) {
-		carPos = 14;
-	} else {
-		if (randVar <= 0.5) {
-			carPos -= 1;
-		} else {
-			carPos += 1;
-		}
-	}
-	console.log(carPos);
-	changeNodeColor(carPos, '#ab78ab');
-	changeNodeColor(prevPos, '#3399ff');
-}, 3000);**/
-
-/**	
-setInterval(function() {
-	prevIterator = iterator;
-	iterator = Math.floor(Math.random()*Dataset.edges.length);
-	console.log(Dataset.edges[iterator]);
-	changeEdgeColor(Dataset.edges[iterator][0],Dataset.edges[iterator][1], '#ab78ab');
-	changeEdgeColor(Dataset.edges[prevIterator][0],Dataset.edges[prevIterator][1], '#3399ff');
-}, 3000);
-**/
 	
 
 
