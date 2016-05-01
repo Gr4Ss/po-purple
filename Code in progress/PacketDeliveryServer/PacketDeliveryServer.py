5d9ba43ff1849b7f1db12acce61ccc234503246f
PieterAppeltans <pieter@appeltans.eu>
2016-05-01 09:41:29 +0200

    Packet delivery server

--- Code in progress/PacketDeliveryServer/PacketDeliveryServer.py
+++ Code in progress/PacketDeliveryServer/PacketDeliveryServer.py
@@ -18,9 +18,12 @@ class PacketDeliveryServer:
         self.restclient.add_team()
         self.map = self.restclient.get_map()
         #self.map = {"vertices": [[1, {"origin": 3, "straight": 2, "left": 4}],[2, {"origin": 1, "straight": 3,"left": 4}],[3, {"origin": 2, "straight": 1, "left": 4}],[4, {"origin": 3, "straight": 1, "left": 2}]],"edges": [[1, 2, 0.3],[1, 3, 0.5],[3, 1, 0.5],[2, 3, 0.1],[3, 4, 0.7],[4, 2, 0.3],[4, 1, 0.8]]}
+        self.real_edges_lenght = generate_real_edges(self.map)
         self.current_position = start_position
         self.current_parcel = None
         self.parcel_picked_up = False
+        self.split_from = None
+        self.status = "NORMAL"
     '''
     Return the edges of the map
     '''
@@ -33,20 +36,106 @@ class PacketDeliveryServer:
         return self.map.get('vertices')
     '''
     Method to update position of the car.
+    Sends new position to rest server and update it locally
     '''
     def update_position(self,position):
+        # Update at restserver
         self.restclient.update_position(position[0],postion[1])
+        # Update locally
         self.current_position = position
     '''
-
+    Method to update the lenght of an edge. This is used as an estimate for the speed on a edge.
     '''
-    def main(self,direction,meta=None):
+    def update_edge_length(self,edge,lenght):
+        if edge in self.real_edges_lenght.keys():
+            # For each edge we keep a list of distance so we can take the median
+            self.real_edges_lenght[edge].append(length)
+    '''
+    Method that return the estimate of the distance of an edge.
+    '''
+    def get_edge_lenght(self,edge):
+        # Determine the length of the edge
+        length = len(self.real_edges_lenght)
+        # Get a sorted copy
+        copy = sorted(self.real_edges_length)
+        # if no element return None
+        if lenght == 0:
+            return None
+        # If even return the mean of the 2 middle elements of the sorted version
+        elif lenght%2 == 0:
+            return (copy[lenght/2 - 1] + copy[lenght/2])*0.5
+        # If odd return the middle element of the sorted version
+        else:
+            return copy[lenght/2]
+    '''
+    Method called when the car is standing at a split. Return the
+    direction (left,right,straight) that the car must turn at the
+    split. The lenght input parameter lenght can be used to pass
+    the lenght of the edge droven since the last call of at_split
+    '''
+    def at_split(self,lenght=None):
+        # Update the length of the current edge
+        if lenght != None:
+            self.update_edge_length(position,lenght)
+        # If the car has no parcel determine new one
         if self.current_parcel == None:
-            new_parcel
-    def new_direction(self,target):
-        path = find_path(self.get_vertices(),self.get_edges(),self.current_position,target)
+            new_parcel()
+            target = self.current_parcel[1]
+        # If we have a goal package but didn't pick it up
+        elif self.parcel_picked_up == False and self.current_position[1] != self.current_parcel[1]:
+            target = self.current_parcel[1]
+        elif self.parcel_picked_up == False:
+            self.parcel_picked_up = True
+            target = self.current_parcel[2]
+        # A parcel is picked up, driving to target
+        elif self.parcel_picked_up == True and self.current_position[1] != self.current_parcel[2]:
+            target = self.current_parcel[2]
+        else:
+            deliver_parcel()
+            new_parcel()
+            if self.current_position[1] != self.current_parcel[1]:
+                target = self.current_parcel[1]
+            else:
+                self.parcel_picked_up = True
+                target  = self.current_parcel[2]
+        self.split_from = self.current_position[0]
+        self.update_position((self.current_position[1],self.current_position[1]))
+        return self.new_direction(self.split_from,self.current_position[1],target)
+    '''
+        Method to call when the car has turned around.
+    '''
+    def turned_arround(self):
+        # New position found by swapping the position elements
+        self.update_position((self.current_position[1],self.current_position[0]))
+    '''
+        Method to check if the car can turn around given its current position
+    '''
+    def can_turn_around(self):
+        # Return True if there there is an edge in the opposite direction
+        for edge in self.get_edges():
+            if edge[0] == self.current_position[1] and edge[1] == self.current_position[0]:
+                return True
+        return False
+    '''
+        Method to call when the car has turned at a split
+    '''
+    def turned(self,direction):
+        new_target = self.to_node_number(self.current_position[1],self.split_from,direction)
+        self.update_position((self.current_position[1],new_target))
+        return self.get_edge_lenght(self.current_position)
+    '''
+        Method that returns the new direction (left,right,straight) at a split
+        frm : node where comming from
+        current: node at which we must turned
+        target: target node
+    '''
+    def new_direction(self,frm,current,target):
+        path = find_path(self.get_vertices(),self.get_edges(),current,target)
         # You are comming from self.current_position[0] at node self.current_position[1] and to path[0]
-        return self.to_left_right_straight(self.current_position[1],self.current_position[0],path[0])
+        return self.to_left_right_straight(current,frm,path[0])
+    '''
+        Method to get new parcel
+    '''
     def new_parcel(self):
         succes = False
         while not succes:
@@ -58,11 +147,21 @@ class PacketDeliveryServer:
                 success = self.restclient.claim_parcel(parcel[0])
             print "ERROR: Couldn't claim parcel"
         self.current_parcel = parcel
+    '''
+        Method to deliver parcels
+    '''
     def deliver_parcel(self):
         self.restclient.deliver_parcel(self.current_parcel[0])
         self.current_parcel = None
         self.parcel_picked_up = False
-
+    '''
+        Method to get info about vertices
+    '''
+    def get_data_node(self,node):
+        for vertice in self.map["vertices"]:
+            if vertice[0] == node:
+                return vertice[1]
+        return None
 
     '''
     Method return 'left', 'right' or 'straight' if you are on the intersect at a given node
@@ -77,7 +176,7 @@ class PacketDeliveryServer:
     In this example the intersect is 4, and you are coming from 5 and you want to go to 1
     then right is returned/
     '''
-    def to_left_right_straight(self,node,From,To):
+    def to_left_right_straight(self,node,frm,to):
         # Example of vertices
         # {"vertices": [[1, {"origin": 3, "straight": 2, "left": 4}],
         #[2, {"origin": 1, "straight": 3,"left": 4}],
@@ -86,12 +185,25 @@ class PacketDeliveryServer:
         #"edges": [[1, 2, 0.3],[1, 3, 0.5],[3, 1, 0.5], ... ]}
         # First of all loop through the vertices to find the right data
         directions = ["origin","right","straight","left"]
-        for vertice in self.map["vertices"]:
-            if vertice[0] == node:
-                node_data = vertice[1]
+        node_data = self.get_data_node(node)
         for key in node_data:
-            if node_data[key] == From:
+            if node_data[key] == frm:
                 from_point = directions.index(key)
-            elif node_data[key] == To:
+            elif node_data[key] == to:
                 end_point = directions.index(key)
         return directions[(end_point-from_point)%len(directions)]
+    def to_node_number(self,node,frm,to):
+        directions = ["origin","right","straight","left"]
+        node_data = self.get_data_node(node)
+        for key,value in node_data.iteritems()::
+            if value == frm:
+                from_direction = directions.index(key)
+                break
+        to_direction = (from_direction+directions.index(to))%len(directions)
+        return node_data[to_direction]
+def  generate_real_edges(mapje):
+    edges = mapje["edges"]
+    result = dict()
+    for edge in edges:
+        result[(edge[0],edge[1])] = []
+    return result
